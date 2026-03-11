e3d57dea55c90b0328074b30146a0ec2fdd0bb2d
Sjain0018 <sj5373557@gmail.com>
2026-03-12 02:35:01 +0530

    Remove import of start selling CTA in home

--- app/page.js
+++ app/page.js
@@ -7,7 +7,6 @@ import HeroSection from "@/components/Home/HeroSection";
 import TrustBanner from "@/components/Home/TrustBanner";
 import HowItWorks from "@/components/Home/HowItWorks";
 import Testimonials from "@/components/Home/Testimonials";
-import StartSellingCTA from "@/components/Home/StartSellingCTA";
 
 export default function HomePage() {
   const [projects, setProjects] = useState([]);
@@ -17,7 +16,6 @@ export default function HomePage() {
       <Header userType="public" isDashboard={false} />
       <main>
         <HeroSection />
-        <StartSellingCTA />
         <HowItWorks />
         <TrustBanner />
         {/* <TrendingProjectsClient /> */}
