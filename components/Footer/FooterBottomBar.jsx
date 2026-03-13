bd7526b09824e019891adf5d55a65124ce455dcd
Sjain0018 <sj5373557@gmail.com>
2026-03-14 01:12:49 +0530

    update footer

--- components/Footer/FooterBottomBar.jsx
+++ components/Footer/FooterBottomBar.jsx
@@ -5,19 +5,8 @@ const FooterBottomBar = () => {
   const currentYear = new Date().getFullYear();
 
   return (
-    <div className="border-t border-gray-200 pt-6 mt-12 flex flex-col sm:flex-row justify-between items-center text-xs text-gray-500 font-sans">
+    <div className="border-t border-gray-200 pt-6 mt-12 flex flex-col sm:flex-row justify-center items-center text-xs text-gray-500 font-sans">
       <p>© {currentYear} ProjectShaala. All rights reserved.</p>
-      <div className="flex space-x-4 mt-2 sm:mt-0">
-        <Link href="/terms" className="hover:text-gray-900 transition">
-          Terms
-        </Link>
-        <Link href="/privacy" className="hover:text-gray-900 transition">
-          Privacy
-        </Link>
-        <Link href="/cookies" className="hover:text-gray-900 transition">
-          Cookies
-        </Link>
-      </div>
     </div>
   );
 };
